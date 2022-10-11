ce9760e9cc48e2c5841ba9736280e21d82083bff
Laura Boetcher <g33klaura@gmail.com>
2022-10-10 21:17:49 -0400

    Add library grid; setup arrays for tiers

--- src/components/TierLists.vue
+++ src/components/TierLists.vue
@@ -1,30 +1,54 @@
 <template>
-  <div class="tiers-grid">
-    <div class="loved row-header">
-      Love it
+  <div class="groot">
+    <h1>My Tiers</h1>
+    <div class="tiers-grid">
+      <div class="loved row-header">
+        Love it
+      </div>
+      <div class="liked row-header">
+        Like it
+      </div>
+      <div class="disliked row-header">
+        Leave it
+      </div>
+      <div class="ondeck row-header">
+        Haven't played
+      </div>
+      <div class="loved-row">Loved these</div>
+      <div class="liked-row">Liked these</div>
+      <div class="disliked-row">Didn't like these</div>
+      <div class="ondeck-row">Haven't played these</div>
     </div>
-    <div class="liked row-header">
-      Like it
+
+    <h2>Games Library</h2>
+    <div class="library-grid">
+      <!-- games go here -->
     </div>
-    <div class="disliked row-header">
-      Leave it
-    </div>
-    <div class="ondeck row-header">
-      Haven't played
-    </div>
-    <div class="loved-row">Loved these</div>
-    <div class="liked-row">Liked these</div>
-    <div class="disliked-row">Didn't like these</div>
-    <div class="ondeck-row">Haven't played these</div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'TierLists',
-    // components: {
+// import draggable from 'vuedraggable';
+import titlesJson from '../assets/titles.json';
 
-    // },
-  }
+export default {
+  name: 'TierLists',
+  components: {
+    // draggable,
+  },
+  data() {
+    return {
+      titles: titlesJson,
+      loved: [],
+      liked: [],
+      disliked: [],
+      ondeck: [],
+    }
+  },
+  methods: {
+    log: function (evt) {
+      window.console.log(evt);
+    }
+  },
+}
 </script>
-
