a838ee51c416b970fb3f81be94b919a3e3c4b37e
Laura Boetcher <g33klaura@gmail.com>
2022-10-10 20:47:02 -0400

    Initial grid layout for tiers

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="tiers-grid">
+      <div class="loved row-header">
+        Love it
+      </div>
+      <div class="liked row-header">
+        Like it
+      </div>
+      <div class="disliked row-header">
+        Leave it
+      </div>
+      <div class="ondeck row-header">
+        Haven't played
+      </div>
+
+      <div class="loved-row">Loved these</div>
+      <div class="liked-row">Liked these</div>
+      <div class="disliked-row">Didn't like these</div>
+      <div class="ondeck-row">Haven't played these</div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+// import HelloWorld from './components/HelloWorld.vue';
 
 import './css/zazz.scss';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
